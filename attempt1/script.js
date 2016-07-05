2fa61670456a9a27d96662554250ad75e61eb7fb
sandgraham <grahammurdoch8@gmail.com>
2016-07-04 17:02:10 -0700

    change to page pan and add page ends

--- attempt1/script.js
+++ attempt1/script.js
@@ -1,8 +1,8 @@
 (function () {
-  var PAGE_TURN_THRESHOLD = .33,
-      PAGE_GAP = 50;
-  var pages = sel('.page'),
-      currentIndex = 0; // this should be set based on selection
+  var PAGE_TURN_THRESHOLD = .2, PAGE_GAP = 50;
+  
+  var pages = sel('.page');
+  var currentIndex = 0; // this should be set based on selection
 
   // prepare each page's position and gesture handling.
   for (var i = 0; i < pages.length; i++) {
@@ -12,6 +12,8 @@
     translate = 'translate3d(' + page.currentX + 'px,0,0)';
     if (i > 0) translate += ' scale(.9)';
     transform(page, translate);
+
+    page.classList.add('animate');
 
     addPageGestures(page);
   }
@@ -36,56 +38,46 @@
     );
 
     pageHammer.on('panstart panmove panend pancancel', function (ev) {
-      panImage(page, ev);
+      panPage(page, ev);
     })
   }
 
-  function panImage(page, ev) {
-    var translate, img = page.querySelector('img');
+  function panPage(page, ev) {
+    var translate;
 
-    // if pan ending, reset to base or turn page
+    // if pan ending, turn page or reset to base position
     if (ev.type == 'panend' || ev.type == 'pancancel') {
-      img.classList.add('animate');
+      page.classList.add('animate');
 
-      // turn page if past threshold or reset
-      if (Math.abs(ev.deltaX) > page.clientWidth * PAGE_TURN_THRESHOLD) {
+      if (shiftOkay(ev.direction) && (Math.abs(ev.deltaX) > page.clientWidth * PAGE_TURN_THRESHOLD)) {
+        // turn page if not end and pan is past threshold
         shiftPages(ev.deltaX);
+      } else {
+        // reset page
+        translate = 'translate3d(0,0,0)';
+        transform(page, translate);
       }
-
-      // reset image
-      translate = 'translate3d(0,0,0)';
-      transform(img, translate);
     }
-    // otherwise move image with pan
+    // otherwise move page with pan
     else {
-      img.classList.remove('animate');
+      page.classList.remove('animate');
       translate = 'translate3d(' + ev.deltaX + 'px, 0, 0)';
-      transform(img, translate);
-    }
-  }
-
-  function shiftLeft () {
-    console.log('move all pages LEFT');
-
-    currentIndex += 1;
-
-    for (var i = 0; i < pages.length; i++) {
-      var translate, newX, page = pages[i];
-
-      page.currentX = page.currentX - (page.clientWidth + PAGE_GAP);
-
-      translate = 'translate3d(' + page.currentX + 'px,0,0)';
       transform(page, translate);
     }
-  };
-
-  function shiftRight () {
-    console.log('move all pages RIGHT');
   }
 
-  function shiftPages (delta) {
+  function shiftOkay(dir) {
+    return currentIndex == 0 && dir === Hammer.DIRECTION_LEFT ||
+      currentIndex == pages.length - 1 && dir == Hammer.DIRECTION_RIGHT;
+  }
+
+  function shiftPages (delta) { 
+    currentIndex -= Math.sign(delta);
+
     for (var i = 0; i < pages.length; i++) {
       var translate, newX, dir, page = pages[i];
+
+      page.classList.add('animate');
 
       shift = Math.sign(delta) * (page.clientWidth + PAGE_GAP); 
       page.currentX = page.currentX + shift;
