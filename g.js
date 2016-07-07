d96d7012926363485e74e274556c9c3009e44e03
sandgraham <grahammurdoch8@gmail.com>
2016-07-06 23:22:13 -0700

    panning and closing functional

--- g.js
+++ g.js
@@ -1,12 +1,24 @@
+var PAN_LIMIT = .2;
 var view = document.querySelector('.view');
 var units = document.querySelectorAll('.unit');
 var frames = document.querySelectorAll('.frame');
 var bodyWidth = document.body.clientWidth;
+var frameWidth = bodyWidth + 100;
+var currentIndex = 0;
+var initialFrame = frames[0];
+var viewHammerManager;
+var unitHammerManagers = [];
 
-// create hammer tap events for all units
+// create hammer tap managers for units or reenable them
 var initGrid = function () {
-	for (var i = 0; i < units.length; i++) {
-		createUnitHammerManager(units[i], i);
+	if (unitHammerManagers.length) {
+		for (var i = 0; i < unitHammerManagers.length; i++) {
+			unitHammerManagers[i].get('tap').set({enable: true});
+		}
+	} else {
+		for (var i = 0; i < units.length; i++) {
+			unitHammerManagers.push(createUnitHammerManager(units[i], i));
+		}
 	}
 }
 
@@ -15,93 +27,103 @@ var createUnitHammerManager = function (unitElem, index) {
 	var unitHammerManager = new Hammer.Manager(unitElem);
 	unitHammerManager.add(new Hammer.Tap());
 	unitHammerManager.on('tap', function () {
-		openGallery(index);
-		// remove handler? or reuse for later
+		if (view.classList.contains('view-grid')) {
+			openGalleryTo(index);
+		}
 	});
+
+	return unitHammerManager;
 }
 
-var openGallery = function (selectedIndex) {
-	var selectedFrame = frames[selectedIndex];
+// close gallery from currentIndex
+var closeGallery = function () {
+	// push closing frame to front
+	initialFrame.classList.remove('frame-selected');
+	frames[currentIndex].classList.add('frame-selected');
 
+	// transition to grid
+	view.classList.remove('view-gallery')
+	view.classList.add('view-grid')
+
+	for (var i = 0; i < frames.length; i++) {
+		frames[i].style.transform = 'none';
+	    frames[i].style.mozTransform = 'none';
+	    frames[i].style.webkitTransform = 'none';
+	}
+
+	viewHammerManager.get('pan').set({enable: false})
+
+	initGrid();
+}
+
+var openGalleryTo = function (selectedIndex) {
+	initialFrame = frames[selectedIndex];
+
+	// give it a z-index to push image to front
+	initialFrame.classList.add('frame-selected');
+
+	// transition to gallery
 	view.classList.add('view-gallery')
 	view.classList.remove('view-grid')
 
-	// give it a z-index to push to front
-	selectedFrame.classList.add('frame-selected');
+	moveGalleryTo(selectedIndex, 0, true);
 
-	moveGalleryTo(selectedIndex);
-}
-
-var moveGalleryTo = function (selectedIndex) {
-	var firstFramePos = (selectedIndex * (bodyWidth + 80)) * -1;
-	for (var i = 0; i < frames.length; i++) {
-		var pos = firstFramePos + (i * (bodyWidth + 80));
-		frames[i].style.transform = 'translate3d(' + pos + 'px,0,0)';
-		frames[i].style.mozTransform = 'translate3d(' + pos + 'px,0,0)';
-		frames[i].style.webkitTransform = 'translate3d(' + pos + 'px,0,0)';
+	// set up manager for panning or reenable it
+	if (viewHammerManager) {
+		viewHammerManager.get('pan').set({enable: true});
+	} else {
+		viewHammerManager = new Hammer.Manager(view);
+	   	viewHammerManager.add(new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10 }));
+	   	viewHammerManager.on('panstart panmove panend pancancel', onPanGallery);
 	}
+
+   	// TODO set up listener for tap: toggle header/footer
 }
 
-// start in grid view, set up tap handler on units
+// move Gallery along x-axis
+var moveGalleryTo = function (showIndex, percent, animate) {
+	showIndex = Math.max(0, Math.min(showIndex, frames.length - 1));
+	percent = percent || 0;
+
+	if (animate) {
+		view.classList.add('animate-frames');
+	} else {
+		view.classList.remove('animate-frames');
+	}
+
+	var pos, translate;
+	for (var i = 0; i < frames.length; i++) {
+		pos = (frameWidth / 100) * (((i - showIndex) * 100) + percent);
+
+		translate = 'translate3d(' + pos + 'px, 0, 0)';
+
+		if (i !== showIndex) {
+			translate += ' scale(.9)'
+		}
+
+		frames[i].style.transform = translate;
+        frames[i].style.mozTransform = translate;
+        frames[i].style.webkitTransform = translate;		
+	}
+
+	currentIndex = showIndex;
+}
+
+var onPanGallery = function (ev) {
+	var delta = ev.deltaX;
+	var percent = (100 / frameWidth) * delta;
+	var animate = false;
+
+	if (ev.type == 'panend' || ev.type == 'pancancel') {
+        if (Math.abs(percent) > PAN_LIMIT && ev.type == 'panend') {
+            currentIndex += (percent < 0) ? 1 : -1;
+        }
+        percent = 0;
+        animate = true;
+    }
+
+    moveGalleryTo(currentIndex, percent, animate);
+}
+
+// start page in grid view
 initGrid();
-
-/*
-
-// currently using body width as the default container for the gallery
-// and the frames class, which assumes absolute pos and full screen
-// this could be generalized into a Gallery object
-
-// todo: change click to contruct a gallery, which keeps track of current index and frame
-$('.frame').on('click', function (ev) {
-	var selectedFrame = this;
-	var selectedIndex = $(selectedFrame.parentNode).index();
-
-	// set view to view-gallery
-	// set frames to absolute and full screen
-	view.classList.add('view-gallery');
-
-	// set selected frame to z-index 1 to emphasize
-	selectedFrame.classList.add('frame-selected');
-
-	// construct gallery, showing selected frame first
-	// btw, reason Carousel from before wasn't working:
-	// assumed a single layer nested structure
-	// if that was changed to just accept a container and an arbitrary set of panes...
-
-	// show header and footer for img, should this be part of moveGalleryTo?
-	// move all frames positions using translate3d
-	moveGalleryTo(selectedIndex);
-
-	// set up listeners
-	// create hammer for horizontal swipe events and taps
-	// for now use arrow keys
-	$(document).on('keyup', function (ev) {
-		if (ev.keyCode === 37) {
-			// left
-			moveGalleryTo(selectedIndex-1);
-			selectedIndex -= 1;
-		}
-		else if (ev.keyCode == 39) {
-			// right
-			moveGalleryTo(selectedIndex+1);
-			selectedIndex += 1;
-		}
-	});
-	// bind cancel event to cancel icon
-	// remove frame-selected now that everything is placed
-	// selectedFrame.classList.remove('frame-selected')
-})
-
-// todo: make this a gallery method
-// calculate first index negative position based on selected frame index
-var moveGalleryTo = function (selectedIndex) {
-	var firstFramePos = (selectedIndex * (bodyWidth + 80)) * -1;
-	for (var i = 0; i < frames.length; i++) {
-		var pos = firstFramePos + (i * (bodyWidth + 80));
-		frames[i].style.transform = 'translate3d(' + pos + 'px,0,0)';
-		frames[i].style.mozTransform = 'translate3d(' + pos + 'px,0,0)';
-		frames[i].style.webkitTransform = 'translate3d(' + pos + 'px,0,0)';
-	}
-}
-
-*/
