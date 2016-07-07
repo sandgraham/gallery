11f568cb67328100a644d66ca8fe9121739b97e9
sandgraham <grahammurdoch8@gmail.com>
2016-07-06 20:32:59 -0700

    start adding hammer gestures

--- g.js
+++ g.js
@@ -1,6 +1,51 @@
 var view = document.querySelector('.view');
+var units = document.querySelectorAll('.unit');
 var frames = document.querySelectorAll('.frame');
 var bodyWidth = document.body.clientWidth;
+
+// create hammer tap events for all units
+var initGrid = function () {
+	for (var i = 0; i < units.length; i++) {
+		createUnitHammerManager(units[i], i);
+	}
+}
+
+// create a hammer manager for the given unit, pass index as a favor
+var createUnitHammerManager = function (unitElem, index) {
+	var unitHammerManager = new Hammer.Manager(unitElem);
+	unitHammerManager.add(new Hammer.Tap());
+	unitHammerManager.on('tap', function () {
+		openGallery(index);
+		// remove handler? or reuse for later
+	});
+}
+
+var openGallery = function (selectedIndex) {
+	var selectedFrame = frames[selectedIndex];
+
+	view.classList.add('view-gallery')
+	view.classList.remove('view-grid')
+
+	// give it a z-index to push to front
+	selectedFrame.classList.add('frame-selected');
+
+	moveGalleryTo(selectedIndex);
+}
+
+var moveGalleryTo = function (selectedIndex) {
+	var firstFramePos = (selectedIndex * (bodyWidth + 80)) * -1;
+	for (var i = 0; i < frames.length; i++) {
+		var pos = firstFramePos + (i * (bodyWidth + 80));
+		frames[i].style.transform = 'translate3d(' + pos + 'px,0,0)';
+		frames[i].style.mozTransform = 'translate3d(' + pos + 'px,0,0)';
+		frames[i].style.webkitTransform = 'translate3d(' + pos + 'px,0,0)';
+	}
+}
+
+// start in grid view, set up tap handler on units
+initGrid();
+
+/*
 
 // currently using body width as the default container for the gallery
 // and the frames class, which assumes absolute pos and full screen
@@ -16,7 +61,7 @@ $('.frame').on('click', function (ev) {
 	view.classList.add('view-gallery');
 
 	// set selected frame to z-index 1 to emphasize
-	selectedFrame.classList.add('frame-selected')
+	selectedFrame.classList.add('frame-selected');
 
 	// construct gallery, showing selected frame first
 	// btw, reason Carousel from before wasn't working:
@@ -58,3 +103,5 @@ var moveGalleryTo = function (selectedIndex) {
 		frames[i].style.webkitTransform = 'translate3d(' + pos + 'px,0,0)';
 	}
 }
+
+*/
